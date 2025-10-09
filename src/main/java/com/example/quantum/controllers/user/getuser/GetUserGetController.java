f2aa8102a2d413c90b0da10974be3ba40a8c0f63
Lucas Moreira Lana <seu.email@exemplo.com>
2025-10-09 16:00:08 -0300

    Adição da anotation '@CrossOrigin' no controller do GetAllUsers para permitir o front acessar a api para buscar as informações requisitadas.

--- src/main/java/com/example/quantum/controllers/user/getuser/GetUserGetController.java
+++ src/main/java/com/example/quantum/controllers/user/getuser/GetUserGetController.java
@@ -2,6 +2,7 @@ package com.example.quantum.controllers.user.getuser;
 
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.example.quantum.domain.User;
 import com.example.quantum.services.user.GetUserGetService;
 
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/v1/users")
 public class GetUserGetController {
