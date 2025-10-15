8e29d7d234b09fed3d8848a080f15234bec83d4f
Lucas Moreira Lana <seu.email@exemplo.com>
2025-10-15 15:45:00 -0300

    Alterações nas classes SecurityConfig, CustomUserDetailsService e SecurityFilter para tentar descobrir erro 403 quando é batida a requisição

--- src/main/java/com/example/quantum/config/SecurityConfig.java
+++ src/main/java/com/example/quantum/config/SecurityConfig.java
@@ -34,7 +34,6 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .anyRequest().authenticated()
                 ).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
                 return http.build();
--- src/main/java/com/example/quantum/infra/security/CustomUserDetailsService.java
+++ src/main/java/com/example/quantum/infra/security/CustomUserDetailsService.java
@@ -2,20 +2,25 @@ package com.example.quantum.infra.security;
 
 import com.example.quantum.repositories.user.UserEntity;
 import com.example.quantum.repositories.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
+    @Autowired
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-        UserEntity entity = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User não encontrado!"));
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity entity = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User não encontrado!"));
         return new org.springframework.security.core.userdetails.User(entity.getEmail(), entity.getPassword(), new ArrayList<>());
     }
-
 }
+
